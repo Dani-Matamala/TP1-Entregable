5cd725b9cedb9df6525aaea9e434e3403f0f99fa
Matamala Daniel <daniel20mat@gmail.com>
2023-04-14 15:46:04 -0300

    modificando la insercion, eliminacion y toString

--- Viaje.php
+++ Viaje.php
@@ -1,12 +1,12 @@
 <?php
 
 class Viaje {
-    private $responsable;//ResponsableV
+    private $responsable; //ResponsableV
     private $codigo;
     private $destino;
     private $maxPasajeros;
-    private $pasajeros;//referencia a coleccion de pasajeros
-    
+    private $pasajeros; //referencia a coleccion de pasajeros
+
     public function __construct($codigo, $destino, $maxPasajeros, $responsable) {
         $this->responsable = $responsable;
         $this->codigo = $codigo;
@@ -14,64 +14,79 @@ class Viaje {
         $this->maxPasajeros = $maxPasajeros;
         $this->pasajeros = array();
     }
-    
+
     public function getCodigo() {
         return $this->codigo;
     }
-    
+
     public function setCodigo($codigo) {
         $this->codigo = $codigo;
     }
-    
+
     public function getDestino() {
         return $this->destino;
     }
-    
+
     public function setDestino($destino) {
         $this->destino = $destino;
     }
-    
+
     public function getMaxPasajeros() {
         return $this->maxPasajeros;
     }
-    
+
     public function setMaxPasajeros($maxPasajeros) {
         $this->maxPasajeros = $maxPasajeros;
     }
-    
+
     public function getPasajeros() {
         return $this->pasajeros;
     }
 
-    public function getResponsableV(){
+    public function getResponsableV() {
         return $this->responsable->__toString();
     }
 
-    public function setResponbleV($responsableV){
+    public function setResponbleV($responsableV) {
         $this->responsable = $responsableV;
     }
-    
-    public function agregarPasajero($nombre, $apellido, $numeroDoc) {
-        if (count($this->pasajeros) < $this->maxPasajeros) {
-            $pasajero = array(
-                "nombre" => $nombre,
-                "apellido" => $apellido,
-                "numeroDoc" => $numeroDoc
-            );
-            array_push($this->pasajeros, $pasajero);
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
-    public function quitarPasajero($numeroDoc) {
-        foreach ($this->pasajeros as $key => $pasajero) {
-            if ($pasajero["numeroDoc"] == $numeroDoc) {
-                unset($this->pasajeros[$key]);
+
+    private function existePasajero($pasajero) {
+        foreach ($this->pasajeros as $buscado) {
+            if ($buscado->getNumero_documento() == $pasajero->getNumero_documento()) {
                 return true;
             }
         }
         return false;
     }
+
+    public function agregarPasajero($pasajero) {
+        if (count($this->pasajeros) < $this->maxPasajeros && !$this->existePasajero($pasajero)) {
+            array_push($this->pasajeros, $pasajero);
+            return true; //caso en que todo salio bien 
+        } else {
+            return false; //caso en que fallo la insercion del objeto pasajero
+        }
+    }
+
+
+    public function quitarPasajero($pasajero) {
+        foreach ($this->pasajeros as $buscado) {
+            if ($buscado->getNumero_documento() == $pasajero->getNumero_documento()) {
+                unset($this->pasajeros[$buscado]);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function __toString() {
+        $pasajeros = "";
+        foreach ($this->pasajeros as $pasajero) {
+            $pasajeros .= $pasajero->__toString() . ", \n";
+        }
+        $pasajeros = rtrim($pasajeros, ', ');
+        return "Código: $this->codigo\nDestino: $this->destino\nMáximo de pasajeros: $this->maxPasajeros\nResponsable: $this->responsable\nPasajeros: $pasajeros";
+    }
+    
 }
